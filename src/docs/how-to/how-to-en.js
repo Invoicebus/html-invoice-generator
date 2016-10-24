8868584093630cfb6da5373ad6b02c870a126d8a
imjustd <dimitarstojanov@yahoo.com>
2016-10-24 15:36:16 +0200

    - Updated English version of 'About' and 'Save State' texts to reflect the new invoice features

--- src/docs/how-to/how-to-en.js
+++ src/docs/how-to/how-to-en.js
@@ -29,7 +29,10 @@ var ib_how_to_data = function(){/*
             &#9675; &nbsp; Auto calculate subtotals, totals, taxes and discounts.
           </ib-div>
           <ib-div class="ib_li">
-            &#9675; &nbsp; Save your default company data for future re-use.
+            &#9675; &nbsp; Save the current invoice data for future re-use.
+          </ib-div>
+          <ib-div class="ib_li">
+            &#9675; &nbsp; Open already saved invoice data (useful when invoicing different clients).
           </ib-div>
         </ib-div>
       </ib-div>
@@ -45,30 +48,41 @@ var ib_how_to_data = function(){/*
     </ib-div>
   </ib-div>
 
-  <ib-div class="ib_h3">How can I save my default company data for future re-use?</ib-div>
-  <ib-div class="ib_p">
-    Click <ib-span class="ib_b">&quot;Save State&quot;</ib-span> button and follow the on-screen instructions.
-  </ib-div>
-
   <ib-div class="ib_h3">How to save the invoice?</ib-div>
   <ib-div class="ib_p">
-    There are two ways to save the invoice:
+    There are three ways to save the invoice:
     <ib-div class="ib_ul">
       <ib-div class="ib_li">
-        - &nbsp; Save it to PDF using PDF printer.
+        1) &nbsp;Save it locally in editable <ib-span class="ib_code">.txt</ib-span> format with the &quot;Save State&quot; button.
       </ib-div>
       <ib-div class="ib_li">
-        - &nbsp; Save it online to our online invoicing system Invoicebus.
+        2) &nbsp;Save it to PDF using PDF printer.
+      </ib-div>
+      <ib-div class="ib_li">
+        3) &nbsp;Save it online to our online invoicing system <ib-span class="ib_a" onclick="window.open('https://invoicebus.com', '_blank')">Invoicebus</ib-span>.
       </ib-div>
     </ib-div>
   </ib-div>
 
   <ib-div class="ib_p">
-    Saving the invoice to PDF can be done by using something called PDF Print Driver. This is also known as printing to PDF because the output from the browser is sent to a file instead to a printer. This functionality is built-in in some browsers (like Google Chrome), while in others (like Firefox or Internet Explorer) you have to install PDF printer manually. A good and free PDF printer is <ib-span class="ib_a" onclick="window.open('http://www.bullzip.com', '_blank')">Bullzip</ib-span>. If you already have your PDF printer installed, <ib-span class="ib_b">click &quot;Print&quot;, set the destination printer to &quot;Save as PDF&quot; and click Save</ib-span>.
+    1) To save the invoice data locally in editable format use the <ib-span class="ib_b">&quot;Save State&quot;</ib-span> button in the top menu and follow the on-screen instructions.
   </ib-div>
 
   <ib-div class="ib_p">
-    In order to save the invoice online you'll need to create an Invoicebus account first. You can do this by clicking &quot;Save Online&quot; button. Then you'll be taken to a secure page where you can complete your registration and save the invoice. Please note that an online invoice is much more powerful than a static PDF invoice. For example, an online invoice can be edited on-the-fly, sent by email, tracked, and paid directly by Credit card, PayPal or Bitcoin. Here's an <ib-span class="ib_a" onclick="window.open('https://invoicebus.com/demoinvoice/', '_blank')">example</ib-span>.
+    2) Saving the invoice to PDF can be done by using something called PDF Print Driver. This is also known as printing to PDF because the output from the browser is sent to a file instead to a printer. This functionality is built-in in some browsers (like Google Chrome), while in others (like Firefox or Internet Explorer) you have to install PDF printer manually. A good and free PDF printer is <ib-span class="ib_a" onclick="window.open('http://www.bullzip.com', '_blank')">Bullzip</ib-span>. If you already have your PDF printer installed, <ib-span class="ib_b">click &quot;Print&quot;, set the destination printer to &quot;Save as PDF&quot; and click Save</ib-span>.
+  </ib-div>
+
+  <ib-div class="ib_p">
+    3) In order to save the invoice online you'll need to create an Invoicebus account first. You can do this by clicking &quot;Save Online&quot; button. Then you'll be taken to a secure page where you can complete your registration and save the invoice. Please note that an online invoice is much more powerful than a static PDF invoice. For example, an online invoice can be edited on-the-fly, sent by email, tracked, and paid directly by Credit card, PayPal or Bitcoin. Here's an <ib-span class="ib_a" onclick="window.open('https://invoicebus.com/demoinvoice/', '_blank')">example</ib-span>.
+  </ib-div>
+
+  <ib-div class="ib_h3">How can I open already saved local invoice data?</ib-div>
+  <ib-div class="ib_p">
+    To open local data, click the <ib-span class="ib_b">&quot;Open&quot;</ib-span> button and from the file browser window select the data file you want to open. Note that the file must be previously saved invoice data with <ib-span class="ib_code">.txt</ib-span> extension, otherwise it won't load.
+  </ib-div>
+
+  <ib-div class="ib_p">
+    Due to browser privacy restrictions the file <ib-span class="ib_b">must be loaded</ib-span> from the same location as the <ib-span class="ib_code">template.html</ib-span> file (the root directory of the template). So if you want to open data file from other location on your computer, first copy it to the template root folder.
   </ib-div>
 
   <ib-div class="ib_h3">How to restyle the template?</ib-div>
@@ -109,7 +123,7 @@ var ib_how_to_data = function(){/*
   </ib-div>
 
   <ib-div class="ib_p">
-    It's fine even if you decide to avoid giving us credit, so we made this choice easier to you. In the <ib-span class="ib_code">data.txt</ib-span> file we included property called <ib-span class="ib_code">invoicebus_fineprint</ib-span> which can be set to <ib-span class="ib_code">true</ib-span> (to show the fine print) or <ib-span class="ib_code">false</ib-span> (to hide the fine print).
+    It's fine even if you decide to avoid giving us credit, so we made this choice easier to you. In the local <ib-span class="ib_code">.txt</ib-span> invoice data file there's a property called <ib-span class="ib_code">invoicebus_fineprint</ib-span> which can be set to <ib-span class="ib_code">true</ib-span> (to show the fine print) or <ib-span class="ib_code">false</ib-span> (to hide the fine print).
   </ib-div>
 
   <ib-div class="ib_h3">Found a bug?</ib-div>
